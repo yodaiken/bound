8f0137ac657967da01bea99650d06026396df311
Aaron Yodaiken <aaron@aryod.com>
2024-09-29 21:59:21 -0400

    include codeowner info

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use chrono::{Duration, Utc};
 use chrono::{FixedOffset, TimeZone};
 use git2::{Commit, Repository};
 use std::collections::HashMap;
+use std::io::Cursor;
 
 use thiserror::Error;
 
 use clap::Parser;
+
 #[derive(Error, Debug)]
 enum BoundError {
     #[error("Not a valid git repository: {0}")]
@@ -34,23 +36,53 @@ fn main() -> Result<(), BoundError> {
 
     revwalk.set_sorting(git2::Sort::TIME)?;
 
-    let mut author_file_stats: HashMap<(String, String), (usize, usize, usize)> = HashMap::new();
+    let mut author_file_stats: HashMap<(String, String, Vec<String>), (usize, usize, usize)> =
+        HashMap::new();
 
     for oid in revwalk {
         let oid = oid?;
         let commit = repo.find_commit(oid)?;
         let author = commit.author().name().unwrap_or("Unknown").to_string();
 
+        let tree = commit.tree()?;
+        let potential_codeowner_paths = vec![".github/CODEOWNERS", "CODEOWNERS", "docs/CODEOWNERS"];
+        let codeowners_contents = potential_codeowner_paths.iter().find_map(|path| {
+            if let Ok(entry) = tree.get_path(std::path::Path::new(path)) {
+                let object = entry.to_object(&repo).ok()?;
+                let blob = object.as_blob()?;
+                let content = std::str::from_utf8(blob.content()).ok()?;
+                Some(content.to_string())
+            } else {
+                None
+            }
+        });
+        let codeowners = if let Some(contents) = codeowners_contents {
+            codeowners::from_reader(Cursor::new(contents))
+        } else {
+            println!(
+                "Warning: No CODEOWNERS file found in commit {}",
+                commit.id()
+            );
+            codeowners::from_reader(Cursor::new("".as_bytes()))
+        };
+
         let file_changes = get_commit_changes(&repo, &commit)?;
         for (file, changes) in file_changes {
+            let owners = codeowners
+                .of(&file)
+                .unwrap_or(&Vec::new())
+                .iter()
+                .map(|owner| owner.to_string())
+                .collect::<Vec<String>>();
             let stats = author_file_stats
-                .entry((author.clone(), file))
+                .entry((author.clone(), file, owners))
                 .or_insert((0, 0, 0));
             stats.0 += 1; // Increment commit count
             stats.1 += changes.additions;
             stats.2 += changes.deletions;
         }
 
+        // Break if since is set and we are past the time
         if let Some(since) = args.since {
             let commit_time = commit.time();
             let commit_date = FixedOffset::east_opt(commit_time.offset_minutes() * 60)
@@ -70,9 +102,11 @@ fn main() -> Result<(), BoundError> {
     Ok(())
 }
 
-fn print_author_file_statistics(stats: &HashMap<(String, String), (usize, usize, usize)>) {
-    for ((author, file), (commits, additions, deletions)) in stats {
-        println!("{}: {}", author, file);
+fn print_author_file_statistics(
+    stats: &HashMap<(String, String, Vec<String>), (usize, usize, usize)>,
+) {
+    for ((author, file, owners), (commits, additions, deletions)) in stats {
+        println!("{}: {} (Owners: {})", author, file, owners.join(", "));
         println!("  C {}  +{}  -{}", commits, additions, deletions);
     }
 }
